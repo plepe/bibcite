3119955961c83c85349441e231218b79b96fd257
Sergey Sergin <kruhak@2840307.no-reply.drupal.org>
2016-07-21 13:55:36 +0600

    make action configurable

--- common/bibcite_export/src/Plugin/Action/ExportBibliography.php
+++ common/bibcite_export/src/Plugin/Action/ExportBibliography.php
@@ -3,9 +3,14 @@
 namespace Drupal\bibcite_export\Plugin\Action;
 
 
-use Drupal\Core\Action\ActionBase;
+use Drupal\bibcite_export\BibciteExportFormatManagerInterface;
+use Drupal\Core\Action\ConfigurableActionBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Export bibliography.
@@ -16,7 +21,52 @@ use Symfony\Component\HttpFoundation\Response;
  *   type = "bibliography"
  * )
  */
-class ExportBibliography extends ActionBase {
+class ExportBibliography extends ConfigurableActionBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Manager of export formats.
+   *
+   * @var \Drupal\bibcite_export\BibciteExportFormatManagerInterface
+   */
+  protected $pluginManager;
+
+  /**
+   * The serializer service.
+   *
+   * @var \Symfony\Component\Serializer\SerializerInterface
+   */
+  protected $serializer;
+
+  /**
+   * Constructs a new ExportBibliography action.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\bibcite_export\BibciteExportFormatManagerInterface $plugin_manager
+   *   The manager of export formats.
+   * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+   *   The serializer service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, BibciteExportFormatManagerInterface $plugin_manager, SerializerInterface $serializer) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->pluginManager = $plugin_manager;
+    $this->serializer = $serializer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition,
+      $container->get('plugin.manager.bibcite_export_format'),
+      $container->get('serializer')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -29,10 +79,7 @@ class ExportBibliography extends ActionBase {
    * {@inheritdoc}
    */
   public function executeMultiple(array $entities) {
-    // @todo Dependency injection.
-    $serializer = \Drupal::service('serializer');
-
-    $content = $serializer->serialize($entities, 'bibtex');
+    $content = $this->serializer->serialize($entities, $this->configuration['format']);
 
     $response = new Response($content);
     $response->headers->set('Cache-Control', 'no-cache');
@@ -50,6 +97,41 @@ class ExportBibliography extends ActionBase {
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
     return $object->access('view', $account, $return_as_object);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'format' => 'bibtex',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $plugins = $this->pluginManager->getDefinitions();
+
+    $form['format'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Format'),
+      '#options' => array_map(function($definition) {
+        return $definition['label'];
+      }, $plugins),
+      '#empty_option' => $this->t('- Select -'),
+      '#requried' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['format'] = $form_state->getValue('format');
   }
 
 }
