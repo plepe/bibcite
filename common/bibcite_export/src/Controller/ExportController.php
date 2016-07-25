487e0c647ac7de7a03cf25f62ddcd5ff9f1a2a22
Sergey Sergin <kruhak@2840307.no-reply.drupal.org>
2016-07-25 10:22:40 +0600

    add simple route for multiple export

--- common/bibcite_export/src/Controller/ExportController.php
+++ common/bibcite_export/src/Controller/ExportController.php
@@ -5,8 +5,11 @@ namespace Drupal\bibcite_export\Controller;
 use Drupal\bibcite_export\BibciteExportFormatManagerInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -17,25 +20,33 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ExportController extends ControllerBase {
 
   /**
-   * Symfony\Component\Serializer\Serializer definition.
+   * The serializer service.
    *
    * @var \Symfony\Component\Serializer\Serializer
    */
   protected $serializer;
 
   /**
-   * Drupal\bibcite_export\BibciteExportFormatManager definition.
+   * The bibcite export format plugin manager.
    *
    * @var \Drupal\bibcite_export\BibciteExportFormatManager
    */
   protected $pluginManager;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(SerializerInterface $serializer, BibciteExportFormatManagerInterface $plugin_manager) {
+  public function __construct(SerializerInterface $serializer, BibciteExportFormatManagerInterface $plugin_manager, EntityTypeManagerInterface $entity_type_manager) {
     $this->serializer = $serializer;
     $this->pluginManager = $plugin_manager;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -44,8 +55,43 @@ class ExportController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('serializer'),
-      $container->get('plugin.manager.bibcite_export_format')
+      $container->get('plugin.manager.bibcite_export_format'),
+      $container->get('entity_type.manager')
     );
+  }
+
+  /**
+   * Serialize and create response contains file in selectrd format.
+   *
+   * @param array $entities
+   *   Array of entities objects.
+   * @param array $bibcite_format
+   *   Format definition.
+   * @param null|string $filename
+   *   Filename. Will be generated if not provided.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   Response object contains serialized bibliography data.
+   */
+  protected function processExport(array $entities, array $bibcite_format, $filename = NULL) {
+    if (!$filename) {
+      $filename = (string) $bibcite_format['label'];
+    }
+
+    $response = new Response();
+
+    if ($result = $this->serializer->serialize($entities, $bibcite_format['id'])) {
+      $response->headers->set('Cache-Control', 'no-cache');
+      $response->headers->set('Content-type', 'text/plain');
+      $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '.' . $bibcite_format['extension'] . '";');
+
+      $response->sendHeaders();
+
+      $result = is_array($result) ? implode("\n", $result) : $result;
+      $response->setContent($result);
+    }
+
+    return $response;
   }
 
   /**
@@ -61,20 +107,44 @@ class ExportController extends ControllerBase {
    * @return \Symfony\Component\HttpFoundation\Response
    *   Response object contains serialized bibliography data.
    */
-  public function export($bibcite_format, $entity_type, EntityInterface $entity) {
-    $response = new Response();
+  public function export(array $bibcite_format, $entity_type, EntityInterface $entity) {
+    $filename = $entity_type . '-' . $entity->id() . (string) $bibcite_format['label'];
+    return $this->processExport([$entity], $bibcite_format, $filename);
+  }
 
-    if ($result = $this->serializer->serialize($entity, $bibcite_format['id'])) {
-      $response->headers->set('Cache-Control', 'no-cache');
-      $response->headers->set('Content-type', 'text/plain');
-      $response->headers->set('Content-Disposition', 'attachment; filename="' . $entity->id() . '.' . $bibcite_format['extension'] . '";');
+  /**
+   * Export multiple entities to available export formats.
+   *
+   * @param array $bibcite_format
+   *   Format definition.
+   * @param string $entity_type
+   *   Entity type identifier.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   Request object.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   Response object contains serialized bibliography data.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+   *   Throw 404 error if Id parameter is not provided or entities not loaded.
+   */
+  public function exportMultiple(array $bibcite_format, $entity_type, Request $request) {
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $storage = $entity_type_manager->getStorage($entity_type);
 
-      $response->sendHeaders();
-
-      $response->setContent(implode("\n", $result));
+    if (!$request->query->has('id')) {
+      throw new NotFoundHttpException();
     }
 
-    return $response;
+    $ids = explode(' ', $request->query->get('id'));
+    $entities = $storage->loadMultiple($ids);
+
+    if (!$entities) {
+      throw new NotFoundHttpException();
+    }
+
+    $filename = $entity_type . '-' . (string) $bibcite_format['label'];
+    return $this->processExport($entities, $bibcite_format, $filename);
   }
 
 }
