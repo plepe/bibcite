15f37355f604b9f6e7eff215f9c4979bc3341a50
Sergey Sergin <kruhak@2840307.no-reply.drupal.org>
2016-08-23 09:10:54 +0600

    Issue #2788361 by discipolo: Add onKernelRequest event subscribers for export formats

--- modules/bibcite_ris/src/EventSubscriber/RisSubscriber.php
+++ modules/bibcite_ris/src/EventSubscriber/RisSubscriber.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\bibcite_bibtex\EventSubscriber;
+namespace Drupal\bibcite_ris\EventSubscriber;
 
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class BibtexSubscriber implements EventSubscriberInterface {
+class RisSubscriber implements EventSubscriberInterface {
 
   /**
    * Register content type formats on the request object.
@@ -16,14 +16,14 @@ class BibtexSubscriber implements EventSubscriberInterface {
    *   The Event to process.
    */
   public function onKernelRequest(GetResponseEvent $event) {
-    $event->getRequest()->setFormat('bibtex', array('application/x-bibtex'));
+    $event->getRequest()->setFormat('ris', ['application/x-research-info-systems']);
   }
 
   /**
    * Implements \Symfony\Component\EventDispatcher\EventSubscriberInterface::getSubscribedEvents().
    */
   public static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = array('onKernelRequest');
+    $events[KernelEvents::REQUEST][] = ['onKernelRequest'];
     return $events;
   }
 
