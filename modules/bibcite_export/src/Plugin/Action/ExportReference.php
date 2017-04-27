ba1b0cd7a7a8e703588fc44b61b773011bc653cc
Sergey Sergin <kruhak@2840307.no-reply.drupal.org>
2017-04-27 13:24:38 +0600

    Issue #2865644: Add action for multiple export references from admin view

--- modules/bibcite_export/src/Plugin/Action/ExportReference.php
+++ modules/bibcite_export/src/Plugin/Action/ExportReference.php
@@ -2,42 +2,40 @@
 
 namespace Drupal\bibcite_export\Plugin\Action;
 
-use Drupal\bibcite\Plugin\BibciteFormatManagerInterface;
 use Drupal\Core\Action\ActionBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\user\PrivateTempStoreFactory;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
 
 /**
- * Export reference.
+ * Export multiple references.
  *
  * @Action(
- *   id = "bibcite_export_action",
+ *   id = "bibcite_export_multiple",
  *   label = @Translation("Export reference"),
  *   type = "bibcite_reference",
- *   deriver = "Drupal\bibcite_export\Plugin\Derivative\ExportFormatDeriver"
+ *   confirm_form_route_name = "entity.bibcite_reference.export_multiple_form",
  * )
  */
 class ExportReference extends ActionBase implements ContainerFactoryPluginInterface {
 
   /**
-   * The serializer service.
+   * The tempstore object.
    *
-   * @var \Symfony\Component\Serializer\SerializerInterface
+   * @var \Drupal\user\PrivateTempStore
    */
-  protected $serializer;
+  protected $tempStore;
 
   /**
-   * Bibcite format manager service.
+   * The current user.
    *
-   * @var \Drupal\bibcite\Plugin\BibciteFormatManagerInterface
+   * @var \Drupal\Core\Session\AccountInterface
    */
-  protected $formatManager;
+  protected $currentUser;
 
   /**
-   * Constructs a new ExportReference action.
+   * Constructs a new ExportReference object.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
@@ -45,25 +43,28 @@ class ExportReference extends ActionBase implements ContainerFactoryPluginInterf
    *   The plugin ID for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Symfony\Component\Serializer\SerializerInterface $serializer
-   *   The serializer service.
-   * @param \Drupal\bibcite\Plugin\BibciteFormatManagerInterface $format_manager
-   *   Bibcite format manager service.
+   * @param \Drupal\user\PrivateTempStoreFactory $temp_store_factory
+   *   The tempstore factory.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   Current user.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, SerializerInterface $serializer, BibciteFormatManagerInterface $format_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PrivateTempStoreFactory $temp_store_factory, AccountInterface $current_user) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
-    $this->serializer = $serializer;
-    $this->formatManager = $format_manager;
+    $this->currentUser = $current_user;
+    $this->tempStore = $temp_store_factory->get('bibcite_export_multiple');
   }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition,
-      $container->get('serializer'),
-      $container->get('plugin.manager.bibcite_format')
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('user.private_tempstore'),
+      $container->get('current_user')
     );
   }
 
@@ -78,28 +79,19 @@ class ExportReference extends ActionBase implements ContainerFactoryPluginInterf
    * {@inheritdoc}
    */
   public function executeMultiple(array $entities) {
-    /** @var \Drupal\bibcite\Plugin\BibciteFormatInterface $format */
-    $format = $this->formatManager->createInstance($this->pluginDefinition['format']);
-    $filename = sprintf('bibcite-multiple-export.%s', $format->getExtension());
-
-    $content = $this->serializer->serialize($entities, $format->getPluginId());
-
-    $response = new Response($content);
-    $response->headers->set('Cache-Control', 'no-cache');
-    $response->headers->set('Content-type', 'application/octet-stream');
-    $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename . '";');
-
-    $response->send();
-
-    // @todo Use route or temporary file to dispatch file to the user.
-    exit();
+    $info = [];
+    /** @var \Drupal\bibcite_entity\Entity\ReferenceInterface $entity */
+    foreach ($entities as $entity) {
+      $info[$entity->id()] = $entity->label();
+    }
+    $this->tempStore->set($this->currentUser->id(), $info);
   }
 
   /**
    * {@inheritdoc}
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
-    return $object->access('view', $account, $return_as_object);
+    return $account->hasPermission('access bibcite export') && $object->access('view', $account, $return_as_object);
   }
 
 }
