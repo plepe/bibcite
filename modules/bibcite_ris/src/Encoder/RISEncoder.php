c40fb810e0a4a8d46a6ecf67b1232a101a63e050
adci_contributor <miu4ia@1830536.no-reply.drupal.org>
2017-12-11 10:14:46 +0600

    Issue #2879865 by camilocodes: Missing bibcite fields for RefWorks-exported RIS

--- modules/bibcite_ris/src/Encoder/RISEncoder.php
+++ modules/bibcite_ris/src/Encoder/RISEncoder.php
@@ -36,6 +36,8 @@ class RISEncoder implements EncoderInterface, DecoderInterface {
      */
     $data = str_replace("\n", "\r\n", $data);
 
+    $config = \Drupal::config('bibcite_entity.mapping.' . $format);
+    $fields = $config->get('fields');
     $ris = new RISReader();
     $ris->parseString($data);
     $records = $ris->getRecords();
@@ -47,9 +49,118 @@ class RISEncoder implements EncoderInterface, DecoderInterface {
           $record[$key] = reset($value);
         }
       }
+      // Additional pages parsing.
+      $pages_string = '';
+      if (array_key_exists('SP', $record) || array_key_exists('EP', $record)) {
+        if (array_key_exists('SP', $record)) {
+          $record['SP'] = (array) $record['SP'];
+        }
+        if (array_key_exists('EP', $record)) {
+          $record['EP'] = (array) $record['EP'];
+        }
+        $max_sp = array_key_exists('SP', $record) ? max($record['SP']) : NULL;
+        $max_ep = array_key_exists('EP', $record) ? max($record['EP']) : 0;
+        if ($max_sp && $max_sp > $max_ep) {
+          $pages_string .= $max_sp . '+';
+          array_splice($record['SP'], array_search($max_sp, $record['SP']), 1);
+        }
+        while ($max_ep) {
+          $pages_string = $max_ep . ', ' . $pages_string;
+          array_splice($record['EP'], array_search($max_ep, $record['EP']), 1);
+          $max_ep = count($record['EP']) > 0 ? max($record['EP']) : NULL;
+          $max_sp = array_key_exists('SP', $record) ? max($record['SP']) : NULL;
+          if ($max_sp && (!$max_ep || $max_sp > $max_ep)) {
+            $pages_string = $max_sp . '-' . $pages_string;
+            array_splice($record['SP'], array_search($max_sp, $record['SP']), 1);
+          }
+        }
+        $record['SP'] = $pages_string;
+        $record['EP'] = $pages_string;
+      }
+
+      // From old format import fix.
+      // User fields to custom.
+      if (isset($record['U1']) && !isset($fields['U1']) && !isset($records['C1'])) {
+        $record['C1'] = $record['U1'];
+        unset($record['U1']);
+      }
+      if (isset($record['U2']) && !isset($fields['U2']) && !isset($records['C2'])) {
+        $record['C2'] = $record['U2'];
+        unset($record['U2']);
+      }
+      if (isset($record['U3']) && !isset($fields['U3']) && !isset($records['C3'])) {
+        $record['C3'] = $record['U3'];
+        unset($record['U3']);
+      }
+      if (isset($record['U4']) && !isset($fields['U4']) && !isset($records['C4'])) {
+        $record['C4'] = $record['U4'];
+        unset($record['U4']);
+      }
+      if (isset($record['U5']) && !isset($fields['U5']) && !isset($records['C5'])) {
+        $record['C5'] = $record['U5'];
+        unset($record['U5']);
+      }
+      // Year of publication.
+      if (isset($record['Y1']) && !isset($fields['Y1']) && !isset($records['PY'])) {
+        $record['PY'] = $record['Y1'];
+        unset($record['Y1']);
+      }
+      // Titles.
+      if ($this->checkKeys(['TI', 'T1', 'ST', 'CT', 'BT'], $record)) {
+        $title = $record['TI'];
+        if ($title === $record['T1'] && $title === $record['ST'] && $title === $record['CT'] && $title === $record['BT']) {
+          unset($record['T1']);
+          unset($record['ST']);
+          unset($record['CT']);
+          unset($record['BT']);
+        }
+      }
+      // Issue.
+      if ($this->checkKeys(['CP', 'IS'], $record) && $record['CP'] === $record['IS']) {
+        unset($record['CP']);
+      }
+      // Short title.
+      if ($this->checkKeys(['J1', 'J2', 'JO'], $record) && $record['J1'] === $record['J2'] && $record['J1'] === $record['JO']) {
+        $record['ST'] = $record['J1'];
+        unset($record['J1']);
+        unset($record['J2']);
+        unset($record['JO']);
+      }
+      // Secondary title.
+      if ($this->checkKeys(['JA', 'JF', 'T2'], $record) && $record['JA'] === $record['JF'] && $record['JA'] === $record['T2']) {
+        unset($record['JA']);
+        unset($record['JF']);
+      }
+      // Abstract.
+      if ($this->checkKeys(['AB', 'N2'], $record) && $record['AB'] && $record['AB'] === $record['N2']) {
+        unset($record['N2']);
+      }
+    }
+
+    if (count($records) === 0) {
+      throw new \Exception(t("Incorrect @format format or empty set.", ['@format' => $format]));
     }
 
     return $records;
+  }
+
+  /**
+   * Check if keys in array.
+   *
+   * @param array $keys
+   *   Array of keys.
+   * @param array $array
+   *   Array to check.
+   *
+   * @return bool
+   *   Result of checking.
+   */
+  private function checkKeys($keys, $array) {
+    $result = TRUE;
+    foreach ($keys as $key) {
+      $result &= array_key_exists($key, $array);
+    }
+    return $result;
   }
 
   /**
@@ -75,23 +186,53 @@ class RISEncoder implements EncoderInterface, DecoderInterface {
   }
 
   /**
-   * Build BibTex entry string.
+   * Build RIS entry string.
    *
    * @param array $data
-   *   Array of BibTex values.
+   *   Array of RIS values.
    *
    * @return string
-   *   Formatted BibTex string.
+   *   Formatted RIS string.
    */
   protected function buildEntry(array $data) {
     $entry = NULL;
-
+    // For not duplicating pages parse.
+    $pages_parsed = FALSE;
     foreach ($data as $key => $value) {
-      if (is_array($value)) {
-        $entry .= $this->buildMultiLine($key, $value);
-      }
-      else {
-        $entry .= $this->buildLine($key, $value);
+      switch ($key) {
+        // Pages found.
+        case "SP":
+        case "EP":
+          if (!$pages_parsed) {
+            $pages_parsed = TRUE;
+            $exp = explode(',', trim($value));
+            foreach ($exp as $page) {
+              // Interval of pages.
+              if (strpos(trim($page), '-') !== FALSE) {
+                $interval = explode('-', trim($page));
+                $entry .= $this->buildLine('SP', $interval[0]);
+                $entry .= $this->buildLine('EP', $interval[count(@$interval) - 1]);
+              } else {
+                // From here to the end.
+                if ($page[strlen(trim($page)) - 1] === '+') {
+                  $entry .= $this->buildLine('SP', trim($page, ' +'));
+                } else {
+                  // Single page.
+                  $entry .= $this->buildLine('EP', trim($page));
+                }
+              }
+            }
+          }
+          break;
+
+        // Not pages.
+        default:
+          if (is_array($value)) {
+            $entry .= $this->buildMultiLine($key, $value);
+          } else {
+            $entry .= $this->buildLine($key, $value);
+          }
+          break;
       }
     }
 
@@ -137,10 +278,10 @@ class RISEncoder implements EncoderInterface, DecoderInterface {
   }
 
   /**
-   * Build the end of Bibtex entry.
+   * Build the end of RIS entry.
    *
    * @return string
-   *   End line for the Bibtex entry.
+   *   End line for the RIS entry.
    */
   protected function buildEnd() {
     return $this->buildLine('ER', '');
